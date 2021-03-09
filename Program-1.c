5b9c45f1d98242356f284697a08c3e4e39efc8d4
mohammedanas677 <20me0102@vvce.ac.in>
2021-03-09 04:19:32 +0000

    n

--- Program-1.c
+++ Program-1.c
@@ -10,7 +10,7 @@ int main()
     int count = 0;
 
     /* Input number from user */
-    printf("Enter any number: ");
+    printf("Enter any number- ");
     scanf("%d", &num);
 
     /* Run loop till num is greater than 0 */
@@ -21,9 +21,9 @@ int main()
 
         /* Remove last digit of 'num' */
         num /= 10;
-    } while(  != 0);
+    } while(num  != 0);
 
-    printf("Total digits: %d", );
+    printf("Total digits- %d",count );
 
     return 0;
 }
